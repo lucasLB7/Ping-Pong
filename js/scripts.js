3dcbf3d589505681581d6009836a0582ad10f379
Luka <plucaslambert@gmail.com>
2018-04-03 09:02:03 +0300

    Style chages

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,24 @@
 //backend
+var pong = 0;
+var ping = 0;
+var pingpong = 0;
 function create_ping(num){
 var numList = [];
+
+
+
   for (i=1; i<=num; i+=1){
     if(i%3===0 && i%5===0){
       numList.push("pingpong");
+      pingpong+=1;
       }
     else if(i%3===0){
       numList.push("Ping");
+      ping+=1;
       }
   else if(i%5===0){
     numList.push("pong");
+      pong+=1;
   }
 
   else {
@@ -20,9 +29,6 @@ var numList = [];
   return numList;
 }
 
-function countPing(numList){
-  
-}
 
 
 
@@ -34,10 +40,9 @@ $(document).ready(function() {
       var numList=create_ping(num);
       numList.forEach(function(item){
         $("ul#output").append("<li>"+item+"</li>");
-        $("#gameBegin").html("<p>The game has begun...Did you guess right???<br>and how many did you guess correctly?<br><br>See with your friends if they can also guess correctly the total abount of divisibles.... </p>");
+
+
       });
-
-
+      $("#count").html("<p>The game has begun...Did you guess right???<br>and how many did you guess correctly?<br><br>See with your friends if they can also guess correctly the total abount of divisibles.... </p><ul><li>"+ping+"</li><li>"+pong+"</li><li>"+pingpong+"</li>");
   });
-
   });
